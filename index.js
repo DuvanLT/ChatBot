8d0a5344c3877ca08e0c0b6b54069f8240711c69
Duvan Mancilla <117489659+DuvanLT@users.noreply.github.com>
2025-07-04 13:41:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 import express from 'express';
-
-
+import { GoogleAuth } from 'google-auth-library';
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+const auth = new GoogleAuth({
+  keyFile: './chatkit-yfkj-9c7b7cfc8088',
+  scopes: 'https://www.googleapis.com/auth/cloud-platform',
+});
+
+const getToken = async () => {
+  const client = await auth.getClient();
+  const tokenResponse = await client.getAccessToken();
+  return tokenResponse.token;
+};
+
+const llamarDialogflow = async (mensaje, sessionId) => {
+  const token = await getToken();
+
+  const res = await fetch(`https://dialogflow.googleapis.com/v2/projects/chatkit-yfkj/agent/sessions/${sessionId}:detectIntent`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      queryInput: {
+        text: {
+          text: mensaje,
+          languageCode: 'es',
+        },
+      },
+    }),
+  });
+
+  const json = await res.json();
 
 // Datos de tu bot
 const BOT_TOKEN = '7640328819:AAEfUWciE45VehMSPDXz7k-8B9zrjqsA9P0';
 const CHAT_ID = '-4687358397'; // el que sacaste de getUpdates
 
-app.use(express.json());
 
 app.post('/webhook', async (req, res) => {
   const intent = req.body.queryResult.intent.displayName;
